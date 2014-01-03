7a4a11a03e90c19153975a46118b52d6861405e0
Tijn <tschmits@gmail.com>
2014-01-03 15:27:47 +0100

    modified js for responsive behaviour

--- parallel_parsets/index.js
+++ parallel_parsets/index.js
@@ -1,17 +1,27 @@
+var w = (w)?w:800;
+var h = (h)?h:600;
+
 var chart = d3.parsets()
-    .dimensions([
-        "Software",
-        "Tools",
-        "Projects/Services"
-    ])
-    .tension(.5)
-    .width(800)
-    .height(400);
+        .dimensions([
+            "Software",
+            "Tools",
+            "Projects/Services"
+        ])
+        .tension(.5);
 
-var vis = d3.select("#viz").append("svg")
-    .attr("width", chart.width())
-    .attr("height", chart.height());
+var svg = d3.select("#viz").append("svg");
 
-d3.csv("except_sp_t_t.csv", function(csv) {
-  vis.datum(csv).call(chart);
-});
+function loaddata(){
+    chart
+        .width(w)
+        .height(h);
+
+    svg
+        .attr("width", chart.width())
+        .attr("height", chart.height());
+
+    d3.csv("except_sp_t_t.csv", function(csv) {
+      svg.datum(csv).call(chart);
+    });
+}
+loaddata();
